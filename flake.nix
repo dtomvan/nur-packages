75232f5b64b9f4af3404770b5d7af7cdd2764ee2
Tom van Dijk <18gatenmaker6@gmail.com>
2025-05-21 18:20:03 +0200

    flake: add check-nur-eval

--- flake.nix
+++ flake.nix
@@ -21,6 +21,33 @@
       {
         packages = pkgs.lib.filterAttrs (name: p: pkgs.lib.isDerivation p) defaultNix;
         formatter = pkgs.nixfmt-tree;
+        # doesn't work like a check because those are builds and are sandboxed
+        # why must flake apps be this way?
+        apps.check-nur-eval =
+          let
+            script = pkgs.writeShellApplication {
+              name = "check-nur-eval";
+              runtimeInputs = with pkgs; [
+                nix
+                jq
+              ];
+              text = ''
+                cd ${./.}
+                nix-env -f . -qa \* --meta \
+                  --allowed-uris https://static.rust-lang.org \
+                  --option restrict-eval true \
+                  --option allow-import-from-derivation true \
+                  --drv-path --show-trace \
+                  -I nixpkgs=${nixpkgs} \
+                  -I ./ \
+                  --json | jq -r 'values | .[].name'
+              '';
+            };
+          in
+          {
+            type = "app";
+            program = "${pkgs.lib.getExe script}";
+          };
       }
     );
 }
