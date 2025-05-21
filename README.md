9fa9d773fea23ee4e2a78852b60abfc8ca7e7f0c
Tom van Dijk <18gatenmaker6@gmail.com>
2025-05-21 18:01:15 +0200

    initial commit, migrate from dtomvan/puntbestanden

---
+++ README.md
@@ -0,0 +1,4 @@
+### Most notable packages
+- Some firefox addons that were built from source
+- [Afio font](https://github.com/awnion/custom-iosevka-nerd-font)
+- My own (very bad) `rwds-cli`, program to learn words with, also already available in [flake](https://github.com/dtomvan/rusty-words)
---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Packages from my personal dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        defaultNix = (import ./. { inherit pkgs; });
+      in
+      {
+        packages = pkgs.lib.filterAttrs (name: p: pkgs.lib.isDerivation p) defaultNix;
+      }
+    );
+}
