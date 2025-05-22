7b3db200c284fc28af07a8a18ccde2c49ec88d8e
Tom van Dijk <18gatenmaker6@gmail.com>
2025-05-22 18:00:07 +0200

    fix: nix flake doesnt contain tsodingPackages

--- flake.nix
+++ flake.nix
@@ -17,17 +17,22 @@
       let
         pkgs = import nixpkgs { inherit system; };
         defaultNix = (import ./. { inherit pkgs; });
-        flakePackages = pkgs.lib.filterAttrs (name: p: pkgs.lib.isDerivation p) defaultNix;
-      in
-      {
-        packages =
+        # HACK: I don't know how to make `nix flake show` not scream at me without doing this
+        flakePackages =
+          with pkgs.lib;
+          mapAttrs (_: p: if !isDerivation p then filterAttrs (_: isDerivation) p else p) (
+            filterAttrs (_: p: isDerivation p || isAttrs p) defaultNix
+          );
+        warnPackages =
           if system != "x86_64-linux" then
-            pkgs.lib.mapAttrs (
-              name: p:
-              pkgs.lib.warn "dtomvan/nur-packages: only x86_64-linux builds are tested, use at own risk" p
-            ) flakePackages
+            pkgs.lib.warn "dtomvan/nur-packages: only x86_64-linux builds are tested, use at own risk" flakePackages
           else
             flakePackages;
+      in
+      {
+        legacyPackages = warnPackages;
+        packages = pkgs.lib.filterAttrs (_: pkgs.lib.isDerivation) warnPackages;
+
         formatter = pkgs.nixfmt-tree;
         # doesn't work like a check because those are builds and are sandboxed
         # why must flake apps be this way?
