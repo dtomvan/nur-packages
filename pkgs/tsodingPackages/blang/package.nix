c12d6cb2c43de656f062f68dd1892280bb5862e5
Tom van Dijk <18gatenmaker6@gmail.com>
2025-06-24 22:12:03 +0200

    tsodingPackages.blang: allow for more targets to be used behind a flag
    It is a bit heavy just to make more tests pass and allow to compile to
    arcane targets.

--- pkgs/tsodingPackages/blang/package.nix
+++ pkgs/tsodingPackages/blang/package.nix
@@ -6,16 +6,35 @@
   fetchFromGitHub,
   nix-update-script,
   makeWrapper,
+  writeShellScriptBin,
 
+  pkgsCross,
+  wineWow64Packages,
+  qemu_full,
   fasm,
   uxn,
-  binutils,
+
+  windowsSupport ? false,
+  aarch64Support ? false,
 }:
 let
-  runtimeDeps = [
-    fasm
-    uxn
-  ] ++ lib.optionals (clangStdenv.hostPlatform.system == "aarch64-linux") [ binutils ];
+  isAarch64 = clangStdenv.hostPlatform.system == "aarch64-linux";
+  runtimeDeps =
+    [
+      fasm
+      uxn
+    ]
+    ++ lib.optionals (!isAarch64 && aarch64Support) [
+      pkgsCross.aarch64-multiplatform.buildPackages.gcc
+      qemu_full
+      # double -> triple
+      (writeShellScriptBin "aarch64-linux-gnu-as" "aarch64-unknown-linux-gnu-as $@")
+      (writeShellScriptBin "aarch64-linux-gnu-gcc" "aarch64-unknown-linux-gnu-gcc $@")
+    ]
+    ++ lib.optionals windowsSupport [
+      pkgsCross.mingwW64.buildPackages.gcc
+      wineWow64Packages.minimal
+    ];
 in
 clangStdenv.mkDerivation {
   # TODO: when Tsoding starts building with nob, use buildNobPackage
@@ -36,6 +55,11 @@ clangStdenv.mkDerivation {
 
   doCheck = true;
   nativeCheckInputs = runtimeDeps;
+
+  preCheck = lib.optionalString windowsSupport ''
+    export WINEPREFIX="$(pwd)/.wine"
+    mkdir -p $WINEPREFIX
+  '';
 
   installPhase = ''
     runHook preInstall
